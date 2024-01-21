9a49eb6c9b80fead05a6df098b8d17a9447867c2
Akhil-2310 <akhilnanavati23@gmail.com>
2024-01-21 22:11:54 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import { WagmiConfig, createConfig } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: "Aqn4xWVZIe3992B7DHpY45WuvQ_Y5S4f", // or infuraId
+    walletConnectProjectId: "8459112bb512644c318d428b28827a55",
+
+    // Required
+    appName: "DeBaGHO",
+  })
+);
+
 
 function App() {
   return (
+     <WagmiConfig config={config}>
+      <ConnectKitProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
     </div>
+    
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
 }
 
